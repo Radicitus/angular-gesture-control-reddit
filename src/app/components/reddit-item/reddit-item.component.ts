f4fb1735d3aec55a621564bf5bb7906c0a05b5ec
Radicitus <novivolatus@gmail.com>
2023-03-22 18:05:38 -0700

    Added full reddit post content from array

--- src/app/components/reddit-item/reddit-item.component.ts
+++ src/app/components/reddit-item/reddit-item.component.ts
@@ -13,20 +13,13 @@ export class RedditItemComponent {
   @Input() post: RedditData;
 
   // Post data
-  title: string = "This is a test title about something interesting";
-  username: string = "testUser";
-  subreddit: string = "r/testSubreddit";
-  thumbnail: string =
-    "https://b.thumbs.redditmedia.com/WxPA-HOO09Ke55Q9GVwyzahaSQSCMX67yyUUbbMmj5s.jpg";
-  upVotes: number = 1002;
-  hasUpvoted: boolean = false;
-  downVotes: number = 45;
-  hasDownvoted: boolean = false;
   postContent: string =
     "This is some post content. It doesn't have to be much, but here it is! Awesome, thanks for reading. There's no way something would be so long but.";
 
   // Post State
   saved: boolean = false;
+  hasUpvoted: boolean = false;
+  hasDownvoted: boolean = false;
 
   // Icons
   upvoteIcon = faArrowUp;
@@ -36,16 +29,6 @@ export class RedditItemComponent {
   constructor() {
     if (this.postContent.length > 88) {
       this.postContent = this.postContent.slice(0, 88) + "...";
-    }
-
-    if (this.post) {
-      // Update post object
-      this.title = this.post.title;
-      // this.username = this.post.username;
-      // this.subreddit = this.post.subreddit;
-      // this.thumbnail = this.post.thumbnail;
-      // this.upVotes = this.post.upvotes;
-      // this.downVotes = this.post.downvotes;
     }
   }
 
@@ -65,13 +48,13 @@ export class RedditItemComponent {
     if (this.hasDownvoted) {
       return;
     } else if (!this.hasDownvoted && !this.hasUpvoted) {
-      this.downVotes++;
+      this.post.downvotes++;
       this.hasDownvoted = true;
     } else {
       this.hasUpvoted = false;
       this.hasDownvoted = true;
-      this.upVotes--;
-      this.downVotes++;
+      this.post.upvotes--;
+      this.post.downvotes++;
     }
   }
 
@@ -79,13 +62,13 @@ export class RedditItemComponent {
     if (this.hasUpvoted) {
       return;
     } else if (!this.hasDownvoted && !this.hasUpvoted) {
-      this.upVotes++;
+      this.post.upvotes++;
       this.hasUpvoted = true;
     } else {
       this.hasUpvoted = true;
       this.hasDownvoted = false;
-      this.upVotes++;
-      this.downVotes--;
+      this.post.upvotes++;
+      this.post.downvotes--;
     }
   }
 }
